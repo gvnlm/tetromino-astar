8522590d4017bfd1d8d6456f79f344e2ea7ff0ec
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-03-10 15:51:35 +1100

    Make Position hashable for use with STL containers

--- Position.h
+++ Position.h
@@ -1,12 +1,28 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+#include <cstddef>
+
 struct Position {
   int x{0};
   int y{0};
 
   bool operator==(Position other) const {
     return x == other.x && y == other.y;
+  }
+
+  std::size_t hash() const {
+    // Implement boost::hash_combine
+    std::size_t seed{static_cast<std::size_t>(x)};
+    seed ^= static_cast<std::size_t>(y) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+
+    return seed;
+  }
+};
+
+struct PositionHash {
+  std::size_t operator()(Position pos) const {
+    return pos.hash();
   }
 };
 
